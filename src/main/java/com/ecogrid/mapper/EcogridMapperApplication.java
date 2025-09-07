3882aa245ad0d586399545e2e15df3643b25dc49
Luis <luishenriqueds01@gmail.com>
2025-09-07 17:38:15 -0300

    feat(main): configuracoes necessarias na main para rodar o projeto

--- src/main/java/com/ecogrid/mapper/EcogridMapperApplication.java
+++ src/main/java/com/ecogrid/mapper/EcogridMapperApplication.java
@@ -1,35 +1,20 @@
 package com.ecogrid.mapper;
 
-import com.ecogrid.mapper.service.GrafoService;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.ecogrid.mapper.facade.GrafoFacade;
+import com.ecogrid.mapper.ui.MenuPrincipal;
 
-@SpringBootApplication
-public class EcogridMapperApplication implements CommandLineRunner {
+public class EcogridMapperApplication {
 
-	private final GrafoService grafoService;
+    public static void main(String[] args) {
+        GrafoFacade grafoFacade = new GrafoFacade();
 
-    public EcogridMapperApplication(GrafoService grafoService){
-        this.grafoService = grafoService;
+        String caminhoSubestacoes = "src/main/resources/data/subestacoes.csv";
+        String caminhoLinhasDeTransmissao = "src/main/resources/data/linhas-de-transmissao.csv";
+        String caminhoAreasProtegidas = "src/main/resources/data/limites_ucs_federais_21072025/limites_ucs_federais_21072025.shp";
+
+        grafoFacade.carregarGrafo(caminhoSubestacoes, caminhoLinhasDeTransmissao, caminhoAreasProtegidas);
+
+        MenuPrincipal menuPrincipal = new MenuPrincipal(grafoFacade);
+        menuPrincipal.iniciar();
     }
-
-    public static void main(String[] args){
-        SpringApplication.run(EcogridMapperApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("Iniciando carregamento do grafo!");
-
-        String caminhoSubestacoes = "src/main/resources/data/subestacoes-data.csv";
-        String caminhoLinhasDeTransmissao = "src/main/resources/data/linhas-de-transmissao-data.csv";
-
-        grafoService.carregarGrafoCompleto(caminhoSubestacoes, caminhoLinhasDeTransmissao);
-
-        System.out.println("Grafo carregado...");
-        System.out.println("Qtd de Subestações: " + grafoService.getQtdSubestacoes());
-        System.out.println("Qtd de linhas: " + grafoService.getQtdLinhas());
-    }
-
 }
